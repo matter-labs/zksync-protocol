4de2322c85dfcb0514c363c05d9deaeddc038308
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2024-09-03 13:32:28 +0200

    Add simple tests with evm interpreter

--- crates/zkevm_test_harness/src/tests/simple_tests/asm_tests.rs
+++ crates/zkevm_test_harness/src/tests/simple_tests/asm_tests.rs
@@ -24,6 +24,31 @@ pub fn run_asm_based_test(
     run_asm_based_test_template(test_dir, &additional_contracts, options, None);
 }
 
+pub fn run_asm_based_test_with_evm_contracts(
+    test_dir: &str,
+    additional_contracts_addresses: &[i32],
+    additional_evm_contracts_addresses:  &[i32],
+    mut options: Options,
+) {
+    let evm_interpreter = compile_additional_contracts(
+        test_dir,
+        &vec![("evm_interpreter".to_owned(), 0)],
+        None
+    )[0].clone().1;
+
+    let other_evm_contracts =additional_evm_contracts_addresses.iter().map(|x| Address::from_low_u64_be(*x as u64)).collect();
+
+    options.evm_interpreter = Some(evm_interpreter);
+    options.other_evm_contracts = other_evm_contracts;
+
+    let additional_contracts = additional_contracts_addresses
+        .iter()
+        .map(|address| (address.to_string(), *address))
+        .collect();
+
+    run_asm_based_test_template(test_dir, &additional_contracts, options, None);
+}
+
 pub fn run_asm_based_test_template(
     test_dir: &str,
     additional_contracts: &Vec<(String, i32)>,
