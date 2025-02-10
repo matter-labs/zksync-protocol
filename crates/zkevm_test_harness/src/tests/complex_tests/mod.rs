8d679e585483c5600e6345e11a6b6182d78ee02c
Vladislav Volosnikov <vladyan18@gmail.com>
2025-02-10 09:42:51 +0100

    fix: Update geometry and generate vks (#110)

--- crates/zkevm_test_harness/src/tests/complex_tests/mod.rs
+++ crates/zkevm_test_harness/src/tests/complex_tests/mod.rs
@@ -431,7 +431,7 @@ fn run_and_try_create_witness_inner(
 
     let circuits_len = basic_block_circuits.len();
     // Number of circuits of a given type.
-    let mut instances_idx = [0usize; 255];
+    let mut instances_idx = [0usize; 256];
 
     for (idx, el) in basic_block_circuits.clone().into_iter().enumerate() {
         let descr = el.short_description();
